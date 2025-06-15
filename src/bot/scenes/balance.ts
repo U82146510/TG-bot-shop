87d8367b310c96acb2603eb7193c4f2082ca2c80
U82146510 <U82146510@protonmail.com>
2025-06-15 13:13:38 +0100

    still working on balance

--- src/bot/scenes/balance.ts
+++ src/bot/scenes/balance.ts
@@ -1,16 +1,15 @@
 import {InlineKeyboard,Bot,Context} from 'grammy';
 import { User } from '../models/User.ts';
 import { logger } from '../logger/logger.ts';
-import { parse } from 'path';
+import {UserFlowState} from '../models/UserFlowState.ts';
 
 export function registerBalanceHandler(bot:Bot<Context>){
     bot.callbackQuery('balance',async(ctx:Context)=>{
         await ctx.answerCallbackQuery();
         const telegramId = ctx.from?.id;
         if(!telegramId) return;
-        let user;
         try {
-            user = await User.findOne({telegramId});
+            const user = await User.findOne({telegramId});
             if(!user) return ctx.answerCallbackQuery({ text: 'User not found.' });
 
             const keyboard = new InlineKeyboard()
@@ -30,21 +29,38 @@ export function registerBalanceHandler(bot:Bot<Context>){
 
     bot.callbackQuery('add_balance',async(ctx:Context)=>{
         await ctx.answerCallbackQuery();
-        await ctx.editMessageText(`To add balance, please select an amount or enter a custom value.\n\n(⚙️ To be implemented...)`)
+        const telegramId = ctx.from?.id;
+        if(!telegramId) return;
+        
+        try {
+            await ctx.editMessageText(`To add balance, please select an amount or enter a custom value.\n\n(⚙️ To be implemented...)`)
+            await UserFlowState.findOneAndUpdate({userId:String(telegramId)},{
+                $set:{
+                    flow:'add_balance',
+                    data:{}
+                }
+            },{ upsert: true})
+        } catch (error) {
+            logger.error('Error at add_balance handler',error);
+        }
     });
 
     bot.on('message:text',async(ctx:Context)=>{
        const telegramId = ctx.from?.id;
        if(!telegramId) return;
+        
+       const flowState = await UserFlowState.findOne({userId:String(telegramId)});
+       if(!flowState || flowState.flow!=='add_balance') return; 
        const input = ctx.message?.text?.trim();
        if(!input){
-            return ctx.reply('please enter the amount');
+            return ctx.reply('Please enter the amount:');
        }
-       const amount = parseFloat(input);
+       const sanitizedInput = input.replace(/[^\d.]/g, '');
+       const amount = parseFloat(sanitizedInput);
        if (isNaN(amount) || amount <= 0) {
             return ctx.reply("❌ Invalid amount. Please enter a positive number (e.g. 0.5).");
        }
-       if (!/^\d+(\.\d{1,8})?$/.test(input)) {
+       if (!/^(\d+|\d+\.\d{1,8}|\.\d{1,8})$/.test(sanitizedInput)) {
             return ctx.reply("❌ Invalid format. Use up to 8 decimal places.");
        }
        if (amount > 1000) {
