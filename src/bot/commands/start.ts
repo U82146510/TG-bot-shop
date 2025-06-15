daa0729794d9c69aa7a6fa08fb24cc27c934cbce
U82146510 <U82146510@protonmail.com>
2025-06-15 10:21:20 +0100

    adding user registration by default at the moment when he joins the bot

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -1,8 +1,17 @@
 import {Bot,Context} from 'grammy';
 import { getMainMenuKeyboard } from '../keyboards/mainMenu.ts';
+import { User } from '../models/User.ts';
+import { logger } from '../logger/logger.ts';
 
 export function registerMainMenu(bot:Bot<Context>){
-    bot.command("start",(ctx:Context)=>{
+    bot.command("start",async(ctx:Context)=>{
+        const telegramId = ctx.from?.id;
+        if(!telegramId) return;
+        const existUser = User.findOne({telegramId});
+        if(!existUser){
+            await User.create({telegramId});
+            logger.info(`New user registered: ${telegramId}`);
+        }
         ctx.reply(`Welcome @${ctx.from?.username||"Anonym"} to Tesseract bot`,{
             reply_markup:getMainMenuKeyboard()
         })
